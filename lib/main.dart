d79422ad4c657afeaf3a27a84cf75d3c142572c6
Stevie Siy <stevie.siy@gmail.com>
2021-05-19 13:11:36 -0500

    Single Sign on Functions, stuck on Settings button press

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ bool isLoggedIn = false;
 bool isNotSetup = true;
 bool isDoorOpen = false;
 bool isConfigured = false;
+bool isBusy = false;
 
 String RPIP = '192.168.1.22:1880';
 
@@ -49,41 +50,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<String> futureDistance;
-  int current = -1, prev = -1, difference;
-  Text output;
-  String messageTitle = "Empty";
-  String notificationAlert = "alert";
-
-  bool isBusy = false;
-  bool isLoggedIn = false;
   String errorMessage;
-
-
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   @override
   Future<void> initState()  {
+    initAction();
     super.initState();
 
-    setupTimedFetch();
+  }
 
-    _firebaseMessaging.configure(
-      onMessage: (message) async{
-        setState(() {
-          messageTitle = message["notification"]["title"];
-          notificationAlert = "New Notification Alert";
-        });
+  void initAction() async {
+    final storedRefreshToken = await secureStorage.read(key: 'refresh_token');
+    if (storedRefreshToken == null) return;
 
-      },
-      onResume: (message) async{
-        setState(() {
-          messageTitle = message["data"]["title"];
-          notificationAlert = "Application opened from Notification";
-        });
+    setState(() {
+      isBusy = true;
+    });
 
-      },
-    );
+    try {
+      final response = await appAuth.token(TokenRequest(
+        AUTH0_CLIENT_ID,
+        AUTH0_REDIRECT_URI,
+        issuer: AUTH0_ISSUER,
+        refreshToken: storedRefreshToken,
+      ));
+
+      final idToken = parseIdToken(response.idToken);
+
+      secureStorage.write(key: 'refresh_token', value: response.refreshToken);
+
+      setState(() {
+        isBusy = false;
+        isLoggedIn = true;
+      });
+    } catch (e, s) {
+      print('error on refresh token: $e - stack: $s');
+      logoutAction();
+    }
+  }
+
+  void logoutAction() async {
+    await secureStorage.delete(key: 'refresh_token');
+    setState(() {
+      isLoggedIn = false;
+      isBusy = false;
+    });
   }
 
   Map<String, dynamic> parseIdToken(String idToken) {
@@ -132,15 +143,6 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  setupTimedFetch() {
-    Timer.periodic(Duration(milliseconds: 1000), (timer) {
-      setState(() {
-        futureDistance = fetchDistance();
-      });
-    });
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -157,41 +159,9 @@ class _MyAppState extends State<MyApp> {
           ? CircularProgressIndicator()
           :!isLoggedIn
             ? Login(loginAction(), errorMessage)
-            : isNotSetup
-            ? Settings()
-              : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              FutureBuilder<String>(
-                future: futureDistance,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                  //return Text(snapshot.data);
-                  current = int.parse(snapshot.data);
-                  difference = current - prev;
-                  if (difference.abs() > 3 && prev != -1) {
-                   showDoorOpen(context);
-                  } else {
-                    output = Text(snapshot.data);
-                  }
-                  prev = current;
-                  return output;
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-          ),
-          Text(
-            notificationAlert,
-          ),
-          Text(
-            messageTitle,
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          ],
-        ),
+              : isNotSetup
+              ? Settings()
+              : Sensor()
         ),
       ),
     );
@@ -284,8 +254,8 @@ class Settings extends StatelessWidget {
             RPIP = value;
           },
           decoration: const InputDecoration(
-              labelText: 'Enter the IP address of the Raspberry Pi, including the Port the Node-RED server is running on:',
-              hintText: 'IP Address:Port'
+              labelText: 'Enter the IP address of the Raspberry Pi',
+              hintText: 'IP Address:Port of Node-RED'
           ),
           validator: (String value) {
             if (value == null || value.isEmpty) {
@@ -302,5 +272,142 @@ class Settings extends StatelessWidget {
         )
       ]
     );
+  }
+}
+
+class Sensor extends StatefulWidget {
+  Sensor({Key key}) : super(key: key);
+
+  @override
+  _SensorState createState() => _SensorState();
+}
+
+class _SensorState extends State<MyApp> {
+  Future<String> futureDistance;
+  int current = -1,
+      prev = -1,
+      difference;
+  Text output;
+  String messageTitle = "Empty";
+  String notificationAlert = "alert";
+  String errorMessage;
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  Future<void> initState()  {
+    super.initState();
+
+    setupTimedFetch();
+
+    _firebaseMessaging.configure(
+      onMessage: (message) async{
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+
+      },
+      onResume: (message) async{
+        setState(() {
+          messageTitle = message["data"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+
+      },
+    );
+  }
+
+  setupTimedFetch() {
+    Timer.periodic(Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        futureDistance = fetchDistance();
+      });
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Locked In',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Locked In'),
+        ),
+        body: Center (
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FutureBuilder<String>(
+                future: futureDistance,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    //return Text(snapshot.data);
+                    current = int.parse(snapshot.data);
+                    difference = current - prev;
+                    if (difference.abs() > 3 && prev != -1) {
+                      showDoorOpen(context);
+                    } else {
+                      output = Text(snapshot.data);
+                    }
+                    prev = current;
+                    return output;
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  // By default, show a loading spinner.
+                  return CircularProgressIndicator();
+                },
+              ),
+              Text(
+                notificationAlert,
+              ),
+              Text(
+                messageTitle,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  showDoorOpen(BuildContext context) async {
+    await Future.delayed(Duration(microseconds: 2));
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('A DOOR HAS BEEN OPENED'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: const <Widget>[
+                  Text('A door sensor has determined a door was opened in your house!'),
+                  Text('Was this you?'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.of(context, rootNavigator: true).pop('dialog');
+                },
+              ),
+              FlatButton(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.of(context, rootNavigator: true).pop('dialog');
+                  isDoorOpen = true;
+                },
+              ),
+            ],
+          );
+        });
   }
 }
